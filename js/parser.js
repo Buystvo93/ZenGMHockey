801a7fe9c60f3075bab5a0deac3df7685b162a61
Buystvo93 <buystvoinc@gmail.com>
2026-01-15 11:08:59 -0500

    Update parser.js

--- js/parser.js
+++ js/parser.js
@@ -1,5 +1,13 @@
 function loadLeague(data) {
+  if (!data || !data.players || !data.teams) {
+    console.error("Invalid league file");
+    return;
+  }
+
+  // Stats page
   renderSkaterStats(data);
   renderGoalieStats(data);
+
+  // Teams page
   renderTeams(data);
 }
