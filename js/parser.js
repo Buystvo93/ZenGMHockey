ca268994e615a57f581597135d9e3cd2d3545f93
Buystvo93 <buystvoinc@gmail.com>
2026-01-15 09:34:27 -0500

    Update parser.js

--- js/parser.js
+++ js/parser.js
@@ -2,6 +2,11 @@ function loadLeague(data) {
   document.getElementById("leagueName").textContent =
     data.meta?.name || "Zen GM League";
 
+  const saved = localStorage.getItem("leagueData");
+  if (saved) {
+    loadLeague(JSON.parse(saved));
+  }
+
   renderStandings(data);
   renderPlayerStats(data);
 }
