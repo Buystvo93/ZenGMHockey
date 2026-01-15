6ca5ea397e4c50064656d22feb792887895a2738
Buystvo93 <buystvoinc@gmail.com>
2026-01-15 11:06:47 -0500

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,11 +1,56 @@
 /* =========================
    SKATER STATS
 ========================= */
+function sumSkaterStats(player) {
+  const totals = { gp: 0, g: 0, a: 0, pts: 0, pm: 0, pim: 0 };
+
+  if (!player.stats) return totals;
+
+  player.stats.forEach(season => {
+    if (!season.gpSkater) return;
+
+    totals.gp += season.gpSkater.gp || 0;
+    totals.g += season.gpSkater.g || 0;
+    totals.a += season.gpSkater.a || 0;
+    totals.pts += season.gpSkater.pts || 0;
+    totals.pm += season.gpSkater.pm || 0;
+    totals.pim += season.gpSkater.pim || 0;
+  });
+
+  return totals;
+}
+
+function sumGoalieStats(player) {
+  const totals = { gp: 0, w: 0, l: 0, sv: 0, gaa: 0, seasons: 0 };
+
+  if (!player.stats) return totals;
+
+  player.stats.forEach(season => {
+    if (!season.gpGoalie) return;
+
+    totals.gp += season.gpGoalie.gp || 0;
+    totals.w += season.gpGoalie.w || 0;
+    totals.l += season.gpGoalie.l || 0;
+    totals.sv += season.gpGoalie.svPct || 0;
+    totals.gaa += season.gpGoalie.gaa || 0;
+    totals.seasons++;
+  });
+
+  return {
+    gp: totals.gp,
+    w: totals.w,
+    l: totals.l,
+    svPct: totals.seasons ? totals.sv / totals.seasons : 0,
+    gaa: totals.seasons ? totals.gaa / totals.seasons : 0
+  };
+}
+
+
 function renderSkaterStats(data) {
   const container = document.getElementById("skaterStats");
   if (!container) return;
 
-  const players = data.players.filter(p => !p.gpG); // skaters only
+  const players = data.players.filter(p => p.pos !== "G");
 
   let html = `
     <table class="stats-table">
@@ -23,17 +68,20 @@ function renderSkaterStats(data) {
 
   players.forEach(p => {
     const team = data.teams[p.tid]?.abbrev || "FA";
+    const s = sumSkaterStats(p);
+
+    if (s.gp === 0) return;
 
     html += `
       <tr>
         <td>${p.firstName} ${p.lastName}</td>
         <td>${team}</td>
-        <td>${p.stats.gp}</td>
-        <td>${p.stats.gpSkater?.g || 0}</td>
-        <td>${p.stats.gpSkater?.a || 0}</td>
-        <td>${p.stats.gpSkater?.pts || 0}</td>
-        <td>${p.stats.gpSkater?.pm || 0}</td>
-        <td>${p.stats.gpSkater?.pim || 0}</td>
+        <td>${s.gp}</td>
+        <td>${s.g}</td>
+        <td>${s.a}</td>
+        <td>${s.pts}</td>
+        <td>${s.pm}</td>
+        <td>${s.pim}</td>
       </tr>
     `;
   });
@@ -41,6 +89,7 @@ function renderSkaterStats(data) {
   html += "</table>";
   container.innerHTML = html;
 }
+
 
 /* =========================
    GOALIE STATS
